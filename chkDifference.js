c94059c0e72fc825dd0737debea2300cc6da65f7
cjm0904 <hfo@nate.com>
2021-02-19 12:39:00 +0900

    Create chkDifference.js

---
+++ chkDifference.js
@@ -0,0 +1,13 @@
+var myObject = $("#myObject");
+
+for (var i = 0; i < 10000; i += 1) {
+  div.append("<p>Hello world</p>");
+}
+
+var myObject= = document.getElementById("myObject");
+
+for (let i = 0; i < 10000; i += 1) {
+  const p = document.createElement("p");
+  p.textContent = "Hello world";
+  div.appendChild(p);
+}
